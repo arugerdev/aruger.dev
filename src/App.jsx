10fb829daf864de8b0f4cde2e74559f92cb5af0f
AlvaroRuCh <alvaroch250@gmail.com>
2023-01-25 16:39:35 +0100

    Fix Web Page icon for light or dark theme

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,28 @@ import SocialNetworks from './components/SocialNetworks'
 import Index from './pages/Index'
 import Projects from './pages/Projects'
 import About from './pages/About'
+import DarkLogo from './assets/images/logo-dark.png'
+import LightLogo from './assets/images/logo-light.png'
+import { useEffect } from 'react'
 function App() {
-  if (window.matchMedia("(prefers-color-scheme: dark)")) {
-    document.head.innerHTML.replace('<link rel="icon" type="image/svg+xml" href="/src/assets/images/logo-dark.png" />', '')
-    document.head.innerHTML += ' <link rel="icon" type="image/svg+xml" href="/src/assets/images/logo-light.png" />'
+
+  const handleUpdateIcon = () => {
+    const DarklinkIconHTML = `<link rel="icon" type="image/svg+xml" href=${DarkLogo} />`
+    const LightlinkIconHTML = `<link rel="icon" type="image/svg+xml" href=${LightLogo} />`
+
+    console.log('useEffect')
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      document.head.innerHTML += DarklinkIconHTML
+    }
+    if (window.matchMedia("(prefers-color-scheme: light)").matches) {
+      document.head.innerHTML += LightlinkIconHTML
+    }
+
+
   }
-  if (window.matchMedia("(prefers-color-scheme: light)")) {
-    document.head.innerHTML.replace('<link rel="icon" type="image/svg+xml" href="/src/assets/images/logo-light.png" />', '')
-    document.head.innerHTML += ' <link rel="icon" type="image/svg+xml" href="/src/assets/images/logo-dark.png" />'
-  }
+
+
+  useEffect(() => handleUpdateIcon())
 
   return (
     <div className="App">
