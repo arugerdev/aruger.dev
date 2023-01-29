2fee220dd908056ff7b25f5cbcf8556391c4a92a
AlvaroRuCh <alvaroch250@gmail.com>
2023-01-29 16:59:25 +0100

    Fix Name Buttons 🛎️

--- src/components/ChangeTheme/index.jsx
+++ src/components/ChangeTheme/index.jsx
@@ -22,7 +22,7 @@ export const ChangeTheme = () => {
   }, [theme])
 
   return (
-    <button onClick={handleClick} className='headerChangeTheme'>
+    <button title='Change Theme' onClick={handleClick} className='headerChangeTheme'>
       <svg clipRule='evenodd' fill='currentColor' fillRule='evenodd' strokeLinejoin='round' strokeMiterlimit='2' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'><path d='m11.998 2c5.517 0 9.997 4.48 9.997 9.998 0 5.517-4.48 9.997-9.997 9.997-5.518 0-9.998-4.48-9.998-9.997 0-5.518 4.48-9.998 9.998-9.998zm0 1.5c-4.69 0-8.498 3.808-8.498 8.498s3.808 8.497 8.498 8.497z' fillRule='nonzero' /></svg>
     </button>
   )
