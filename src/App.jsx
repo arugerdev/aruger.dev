9c50bc89a02066417fb450dcd12feb57c265046c
AlvaroRuCh <alvaroch250@gmail.com>
2023-03-01 19:06:01 +0100

    Remove web icon for better performance 🏍️

--- src/App.jsx
+++ src/App.jsx
@@ -9,27 +9,12 @@ import SocialNetworks from './components/SocialNetworks'
 import Index from './pages/Index'
 import Projects from './pages/Projects'
 import About from './pages/About'
-import DarkLogo from './assets/images/logo-dark.png'
-import LightLogo from './assets/images/logo-light.png'
-import { useEffect } from 'react'
 import Blog from './pages/Blog'
 import Blogs from './components/Blogs'
 import getAllBlogsCards from './getAllBlogsCards'
 function App () {
   const [location] = useLocation()
   const blogId = parseInt((location.includes('/blog/') ? location.replace('/blog/', '') : '0'))
-  const handleUpdateIcon = () => {
-    const DarklinkIconHTML = `<link rel="icon" type="image/svg+xml" href=${DarkLogo} />`
-    const LightlinkIconHTML = `<link rel="icon" type="image/svg+xml" href=${LightLogo} />`
-    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-      document.head.innerHTML += DarklinkIconHTML
-    }
-    if (window.matchMedia('(prefers-color-scheme: light)').matches) {
-      document.head.innerHTML += LightlinkIconHTML
-    }
-  }
-
-  useEffect(() => handleUpdateIcon())
 
   return (
     <div className='App'>
