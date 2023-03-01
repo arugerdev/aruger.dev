fb71ba6a76c917e250d82f34e83cde2717902d9e
AlvaroRuCh <alvaroch250@gmail.com>
2023-03-01 18:58:48 +0100

    Better wouter practices 👌

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,14 @@ import './pages/Blog/Blog.css'
 import './pages/Index/IndexPage.css'
 import './pages/Projects/Projects.css'
 import { Header } from './components/Header'
-import { Route, useLocation } from 'wouter'
+import { Route, useLocation, Switch, Router } from 'wouter'
 import SocialNetworks from './components/SocialNetworks'
 import Index from './pages/Index'
 import Projects from './pages/Projects'
 import About from './pages/About'
 import DarkLogo from './assets/images/logo-dark.png'
 import LightLogo from './assets/images/logo-light.png'
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import Blog from './pages/Blog'
 import Blogs from './components/Blogs'
 import getAllBlogsCards from './getAllBlogsCards'
@@ -31,24 +31,19 @@ function App () {
 
   useEffect(() => handleUpdateIcon())
 
-  const [loading, setLoading] = useState(true)
-
-  useEffect(() => {
-    setLoading(false)
-  }, [])
-
   return (
     <div className='App'>
       <Header />
       <div className='marginTop' />
-      {(!loading) &&
-        <>
+      <Router>
+        <Switch>
           <Route path='/' component={Index} />
           <Route path='/projects' component={Projects} />
           <Route path='/about' component={About} />
           <Route path='/blog' component={Blog} />
           <Route path='/blog/:id'> <Blogs id={blogId} data={getAllBlogsCards()[blogId].data} /></Route>
-        </>}
+        </Switch>
+      </Router>
       <SocialNetworks />
     </div>
   )
